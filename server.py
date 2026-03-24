2d5a68b900ffc21baec3d147ac945b37db3fdf14
Lnly77 <aleksandrkondratyuk31@gmail.com>
2026-03-24 20:31:25 +0100

    Update server.py

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ def start_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(('0.0.0.0', 5000))
     server.listen(5)
-    print("server is running on port 5000...")
+    print("server is running on port 5000...") # running
 
     while True:
         client, addr = server.accept()
